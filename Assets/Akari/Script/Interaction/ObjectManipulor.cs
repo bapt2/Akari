94c520b077b7f204df8831cce0ecd39b8b07596b
KenzaDigital <kenza.chabane4@gmail.com>
2025-07-18 14:10:27 +0200

    GrapObject

--- Assets/Akari/Script/Interaction/ObjectManipulor.cs
+++ Assets/Akari/Script/Interaction/ObjectManipulor.cs
@@ -2,15 +2,67 @@ using UnityEngine;
 
 public class ObjectManipulor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Grabbing Settings")]
+    public Transform grabPoint; // Point devant la caméra
+    public float grabDistance = 3f;
+    public LayerMask interactableLayer; 
 
-    // Update is called once per frame
+    private GameObject heldObject;
+    private Rigidbody heldRb;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObject == null)
+                TryGrab();
+            else
+                Drop();
+        }
+
+        if (heldObject != null)
+        {
+            heldRb.MovePosition(grabPoint.position);
+        }
+
+        if (Input.GetMouseButtonDown(0) && heldObject != null)
+        {
+            Throw();
+        }
+    }
+
+    void TryGrab()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, grabDistance, interactableLayer))
+        {
+            heldObject = hit.collider.gameObject;
+            heldRb = heldObject.GetComponent<Rigidbody>();
+            if (heldRb != null)
+            {
+                heldRb.useGravity = false;
+                heldRb.freezeRotation = true;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        if (heldRb != null)
+        {
+            heldRb.useGravity = true;
+            heldRb.freezeRotation = false;
+        }
+        heldObject = null;
+        heldRb = null;
+    }
+
+    void Throw()
+    {
+        heldRb.useGravity = true;
+        heldRb.freezeRotation = false;
+        heldRb.AddForce(transform.forward * 500f);
+        heldObject = null;
+        heldRb = null;
     }
 }
