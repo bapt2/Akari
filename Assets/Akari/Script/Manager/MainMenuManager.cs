a50f1d3d9978a0c3c7be37312491bb3d90c17bd7
KenzaDigital <kenza.chabane4@gmail.com>
2025-07-21 14:19:18 +0200

    PrefabUpdate+MenuControlAdd

--- Assets/Akari/Script/Manager/MainMenuManager.cs
+++ Assets/Akari/Script/Manager/MainMenuManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject MainMenuPanel;
+    public GameObject ControlPanel;
+
     public void StartButton(int sceneIndex)
     {
         LevelLoader.instance.LoadLevel(sceneIndex);
@@ -10,5 +14,17 @@ public class MainMenuManager : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void ShowControls()
+    {
+        MainMenuPanel.SetActive(false);
+        ControlPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        ControlPanel.SetActive(false);
+        MainMenuPanel.SetActive(true);
     }
 }
